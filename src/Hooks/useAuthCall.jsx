33ea8c19348414080941fb9ca86e4dee9917ee5f
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-04-02 00:41:17 +0300

    v-1

--- src/Hooks/useAuthCall.jsx
+++ src/Hooks/useAuthCall.jsx
@@ -2,89 +2,70 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess } from "../features/authSlice";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 
 const useAuthCall = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
     const BASE_URL = "https://12126.fullstack.clarusway.com/"
  const login = async(userInfo)=>{
     
-    
    dispatch(fetchStart())
     try {
-         const {data} = await axios.post(`${BASE_URL}account/auth/login/`,
-         userInfo
-         );
-
-         dispatch(loginSuccess(data));
-
+         const {data} = await axios.post(`${BASE_URL}account/auth/login/`,userInfo)
+         dispatch(loginSuccess(data))
+         toastSuccessNotify("Login performed")
          navigate("/stock")
-
-         console.log(data);
-
-
+         console.log(data)
+         return data
     } catch (error) {
-
         dispatch(fetchFail())
-
         console.log(error)
     }
 
+  
 }
-const register = async(userInfo)=>{
+ const register = async(userInfo)=>{
     
-   
+
    dispatch(fetchStart())
     try {
-         const {data} = await axios.post(
-            `${BASE_URL}account/account/register`,
-         userInfo
-         );
-
-         dispatch(registerSuccess(data));
-
-         navigate("/stock");
-
-         console.log(data);
-
-      
-
+         const {data} = await axios.post(`${BASE_URL}account/register/`,userInfo)
+         dispatch(registerSuccess(data))
+         toastSuccessNotify("Register performed")
+         navigate("/stock")
+         console.log(data)
+         return data
     } catch (error) {
-
         dispatch(fetchFail())
-
+        toastErrorNotify("Register can not be performed")
         console.log(error)
     }
 
+  
 }
-const logout = async()=>{
+ const logout = async()=>{
     
     
-   dispatch(fetchStart());
-
+   dispatch(fetchStart())
     try {
-         const {data} = await axios.post(
-            `${BASE_URL}account/auth/logout/`,
-      
-         );
-
-         dispatch(logoutSuccess(data));
-
+         const {data} = await axios.post(`${BASE_URL}account/auth/logout/`)
+         dispatch(logoutSuccess())
+         toastSuccessNotify("Logout performed")
          navigate("/")
-
-         console.log(data);
-
-
-
+         console.log(data)
+         return data
     } catch (error) {
-
         dispatch(fetchFail())
-
+        toastErrorNotify("Logout can not be performed")
         console.log(error)
     }
 
+  
 }
+
 return {login,register,logout}
 
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,36 +5,20 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Formik, Form } from "formik";
-import TextField from "@mui/material/TextField";
-import {object,string,number,date,InferType} from "yup";
-import LoadingButton from '@mui/lab/LoadingButton';
-import { login } from "../Hooks/useAuthCall";
-import useAuthCall from "../Hooks/useAuthCall";
+import { Formik } from "formik"
+import LoginForm, { loginScheme } from "../components/LoginForm";
+import useAuthCall from "../Hooks/useAuthCall"
+
+
+
+
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { currentUser, error,loading } = useSelector((state) => state?.auth);
-
-  const {login} = useAuthCall()
-
-
-  const LoginScheme =object({
-    email: string()
-      .email("Lutfen valid bir email giriniz")
-      .required("Email zorunludur"),
-    password: string()
-      .required("password zorunludur")
-      .min(8, "password en az 8 karakter olmalıdır")
-      .max(20, "password en fazla 20 karakter olmalıdır")
-      .matches(/\d+/,"Password bir sayı içermelidir")
-      .matches(/[a-z]/,"Password en az bir küçük harf içermelidir")
-      .matches(/[A-Z]/,"Password en az bir büyük harf içermelidir")
-      .matches(/[!,?{}><%&$#+-.]+/,"Password en az bir  özel harf içermelidir")
-  })
-  return (
+  const { login } = useAuthCall()
+ 
+   return (
     <Container maxWidth="lg">
       <Grid
         container
@@ -71,48 +55,19 @@ const Login = () => {
             Login
           </Typography>
           <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={LoginScheme}
-            onSubmit={(values, actions) => {
-              login(values);
-              actions.resetForm();
-              actions.setSubmitting(false);
-            }}
+          initialValues={{email:"" ,password:""}}
+          validationSchema={loginScheme}
+          onSubmit={(values,actions)=>{
+            login(values)
+            actions.resetForm();
+            actions.setSubmitting(false);
+          }}
+          component={(props)=><LoginForm{...props}/>}
           >
-            {({ values, handleChange, handleBlur, errors, touched }) => (
-              <Form>
-                <Box sx={{ display: "flex", flexDirection: "column", gap:2 }}>
-                <TextField
-                    label="Email"
-                    name="email"
-                    id="email"
-                    type="email"
-                    variant="outlined"
-                    value={values?.email || ""}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.email && Boolean(errors.email)}
-                    helperText={touched.email && errors.email}
-                  />
-                  <TextField
-                    label="Password"
-                    name="password"
-                    id="password"
-                    type="password"
-                    variant="outlined"
-                    value={values?.password || ""}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.password && Boolean(errors.password)}
-                    helperText={touched.password && errors.password}
-                  />
-                  <LoadingButton type="submit" variant="contained" loading={loading}>
-                        submit
-                  </LoadingButton>
-                </Box>
-              </Form>
-            )}
+          
+
           </Formik>
+
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
           </Box>
