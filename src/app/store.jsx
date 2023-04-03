3a10f1125150ab535d144e2a1d9e2b18c4f93c1a
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-04-03 23:04:22 +0300

    v-3

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,9 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
 
-import { persistStore, persistReducer } from 'redux-persist'
+
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
- 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist" 
 
  
 const persistConfig = {
@@ -19,7 +28,14 @@ const persistedReducer = persistReducer(persistConfig, authReducer)
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock:stockReducer,
   },
+  middleware: (getDefaultMiddleware) => //uygunlaştırma yapan ara birim//
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Button } from "@mui/material"
+import Typography from "@mui/material/Typography"
+import { useEffect } from "react"
+
+import useStockCall from "../Hooks/useStockCall"
+
+// import axios from "axios"
+// import { useDispatch, useSelector } from "react-redux"
+// import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
 
 const Firms = () => {
+  // const { token } = useSelector((state) => state.auth)
+  // const dispatch = useDispatch()
+
+  // const getFirms = async () => {
+  //   const BASE_URL = "https://12126.fullstack.clarusway.com/"
+  //   dispatch(fetchStart())
+  //   const url = "firms"
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     })
+  //     dispatch(getSuccess({ data, url }))
+  //   } catch (error) {
+  //     console.log(error)
+  //     dispatch(fetchFail())
+  //   }
+  // }
+
+const {getStockData} = useStockCall()
+
+
+
+
+  useEffect(() => {
+    // getFirms()
+   getStockData("firms")
+  }, [])
+
+ 
+
   return (
     <div>
-      FIRMS
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+
+      <Button
+        variant="contained"
+       
+      >
+        New Firm
+      </Button>
+
+  
+   
     </div>
   )
 }
